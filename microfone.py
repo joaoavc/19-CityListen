0c96b06ca1cc858963aaedcd65e5a6ab2eaceff9
joaoavc <101803410+joaoavc@users.noreply.github.com>
2022-05-22 15:48:45 +0100

    sistema basico funcional

--- microfone.py
+++ microfone.py
@@ -11,16 +11,8 @@ from sound_detection import *
 def listen():
     fs = 44100  # Sample rate
     seconds = 3  # Duration of recording
-    seconds_analyze = 0.1  # Duration of recording
-    myrecording = None
-
-    while(True):
-        analyze_myrecording = sd.rec(int(seconds_analyze * fs), samplerate=fs, channels=2)
-
-        if(analyze_rms(analyze_myrecording, 10)):
-            myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-            sd.wait()  # Wait until recording is finished
-            print("RECORD")
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
     return myrecording
 
 if __name__ == "__main__":
