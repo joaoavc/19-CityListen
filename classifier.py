48d0702f309108a28f3feaf63cb8551b765ca24a
joaoavc <101803410+joaoavc@users.noreply.github.com>
2022-05-29 23:46:01 +0100

    =Testing sound event detection

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@
 import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+import time
 
 
 
@@ -10,5 +11,7 @@ def classifier(features):
     model = pickle.load(open('decision_tree.p','rb'))
     scaler = StandardScaler()
     #X = scaler.fit_transform(features)
+    start_time = time.time()
     predict = model.predict(features)
+    print("--- %s seconds ---" % (time.time() - start_time))
     return predict
